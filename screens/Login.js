0f757a889f5f5b5be4229369b489d7f1777e80a4
Kofi Boakye <kofiboakye2002@gmail.com>
2021-10-29 21:41:22 +0000

    second commit

--- screens/Login.js
+++ screens/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import {AntDesign} from '@expo/vector-icons'
 
-export default function Login() {
+export default function Login({navigation}) {
     return (
     <View style={{flex: 1, backgroundColor: 'black', justifyContent: "center", alignItems: "center"}}>
         <Image 
@@ -13,8 +13,11 @@ export default function Login() {
                 marginBottom: 30
         }}
         source={{uri: "https://coloringhome.com/coloring/9ip/bap/9ipbapd5T.jpg"}} />
-        <Text style={{ color: "white", fontSize: 18}}>Welcome to the Sneaker Shopping App</Text>
+        <Text style={{ color :"#4eb3fa", fontSize: 18}}>Welcome to the Sneaker Shopping App</Text>
         <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Home")
+          }}
             style={{ backgroundColor: "white", 
             padding: 10,
             paddingHorizontal: 70,
@@ -27,6 +30,9 @@ export default function Login() {
                 <Text style={{fontSize: 15, color: 'black', marginLeft: 15}}>Login with Gmail</Text>
         </TouchableOpacity>
         <TouchableOpacity
+          onPress={() => {
+              navigation.navigate("Home")
+          }}
             style={{ 
             backgroundColor: "#e5e4e4", 
             padding: 10,
@@ -38,6 +44,12 @@ export default function Login() {
         >
             <AntDesign name="apple1" size={24} color="black" />
                 <Text style={{fontSize: 15, color: 'black', marginLeft: 15}}>Login with Apple</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
+        <Text style={{fontSize: 15, marginTop: 10, fontWeight: 'bold', color :'#f6f4f4'}}>
+            Not a member? <Text style={{color :"#4eb3fa", fontWeigth: 'bold' }}>Signup</Text>
+            </Text>
         </TouchableOpacity>
     </View>
     );
