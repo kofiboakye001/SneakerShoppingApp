053d7bb8dff0f1bcf55b527bc335c338409f08b3
Kofi Boakye <kofiboakye2002@gmail.com>
2021-11-13 11:14:28 +0000

    7th commit

--- screens/Home.js
+++ screens/Home.js
@@ -2,6 +2,8 @@ import React, { useRef } from 'react';
 import { View, TouchableOpacity, Text, Image, StyleSheet, Animated, Dimensions, SafeAreaView, TextInput} from "react-native";
 import { ImageHeaderScrollView, TriggeringView } from 'react-native-image-header-scroll-view';
 import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 const MIN_HEIGHT = Platform.OS === 'ios' ? 90 : 55;
 const MAX_HEIGHT = 130
@@ -18,6 +20,11 @@ export default function Home({navigation}) {
              minHeight={MIN_HEIGHT}
              renderHeader={() => (
                  <Image source={{uri: "https://i.pinimg.com/originals/4c/52/97/4c5297b1a347d051533c39e3f630a274.jpg"}} style={styles.image} />
+             )}
+             renderForeground={() => (
+               <View style={styles.titleContainer} >
+                 <Text style={styles.imageTitle}> SNEAKER SHOP</Text>
+               </View>
              )}
             >
                 <TriggeringView style={styles.section}>
@@ -46,14 +53,142 @@ export default function Home({navigation}) {
       </SafeAreaView>
                     </View>
                 </TriggeringView>
-                <View>
-                    <Image style={{
-            width: 500,
-            height: 300,
-            marginLeft: -50,
-        }} source={require('../assets/images/shoe1.png')} />
-                </View>
+                
             </ImageHeaderScrollView>
+            <View style={{backgroundColor: 'white'}}>
+            <View style={{ flexDirection: 'row', justifyContent: "space-between", alignItems: 'center', marginTop: 10}}>
+                <TouchableOpacity 
+            style={{ backgroundColor: "black", 
+            padding: 5,
+            marginLeft: 20,
+            height: 180,
+            paddingHorizontal: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 23,
+            }}>
+              <MaterialIcons style={{marginBottom:130}} name="favorite-border" size={24} color="white" />
+             <Text style={{color: 'white', marginTop: 40, }}>Air Force 1 Low</Text>
+            <Image style={{
+            marginBottom: 80,  
+            width: 150,
+            height: 110,
+            marginLeft: -125,
+            }} 
+            source={require('../assets/images/Af1.png')} />
+           </TouchableOpacity>
+           
+           <TouchableOpacity 
+            style={{ backgroundColor: "#dedfe1", 
+            padding: 5,
+            height: 180,
+            paddingHorizontal: 25,
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 23,
+            marginLeft: -20,
+            marginRight: 20,
+            }}>
+              <MaterialIcons style={{marginBottom:130, marginLeft: -15}} name="favorite-border" size={24} color="white" />
+              <Text style={{color: 'white', marginTop: 40, }}>Nike SB Dunk Low</Text>
+              <Text style={{color: 'white', marginTop: 80, marginLeft: -125, marginRight: 10}}>Pro FTC Lagoon Pulse</Text>
+            <Image style={{
+          width: 120,
+          height: 100,
+          marginBottom: 65,
+          marginLeft: -140,
+        }} source={require('../assets/images/ftc-lagoon.png')} />
+           </TouchableOpacity>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: "space-between", alignItems: 'center', marginTop: 10}}>
+                <TouchableOpacity 
+            style={{ backgroundColor: "#f6d9b8", 
+            padding: 5,
+            height: 180,
+            paddingHorizontal: 25,
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 23,
+            marginLeft: 20,
+            marginRight: 20,
+            }}>
+              <MaterialIcons style={{marginBottom:130, marginLeft: -15}} name="favorite-border" size={24} color="white" />
+              <Text style={{color: 'white', marginTop: 40, }}>Nike SB Dunk Low</Text>
+              <Text style={{color: 'white', marginTop: 80, marginLeft: -85, marginRight: 20}}>Georgetown</Text>
+            <Image style={{
+          width: 130,
+          height: 110,
+          marginBottom: 65,
+          marginLeft: -140,
+        }} source={require('../assets/images/georgetown.png')} />
+           </TouchableOpacity>
+           <TouchableOpacity 
+            style={{ backgroundColor: "#fbb0a3", 
+            padding: 5,
+            marginLeft: 0,
+            marginRight: 15,
+            height: 180,
+            paddingHorizontal: 15,
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 23,
+            }}>
+              <MaterialIcons style={{marginBottom:130}} name="favorite-border" size={24} color="white" />
+             <Text style={{color: 'white', marginTop: 40, }}>Air Force 1 Low</Text>
+            <Image style={{
+            marginBottom: 80,  
+            width: 150,
+            height: 90,
+            marginLeft: -125,
+            }} 
+            source={require('../assets/images/tokyo.png')} />
+           </TouchableOpacity>
+           <View style={{ flexDirection: 'row', justifyContent: "space-between", alignItems: 'center', marginTop: 10}}>
+                <TouchableOpacity 
+            style={{ backgroundColor: "#f6d9b8", 
+            padding: 5,
+            height: 180,
+            paddingHorizontal: 25,
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 23,
+            marginLeft: 20,
+            marginRight: 20,
+            }}>
+              <MaterialIcons style={{marginBottom:130, marginLeft: -15}} name="favorite-border" size={24} color="white" />
+              <Text style={{color: 'white', marginTop: 40, }}>Nike SB Dunk Low</Text>
+              <Text style={{color: 'white', marginTop: 80, marginLeft: -85, marginRight: 20}}>Georgetown</Text>
+            <Image style={{
+          width: 130,
+          height: 110,
+          marginBottom: 65,
+          marginLeft: -140,
+        }} source={require('../assets/images/georgetown.png')} />
+           </TouchableOpacity>
+           <TouchableOpacity 
+            style={{ backgroundColor: "#fbb0a3", 
+            padding: 5,
+            marginLeft: 0,
+            marginRight: 15,
+            height: 180,
+            paddingHorizontal: 15,
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 23,
+            }}>
+              <MaterialIcons style={{marginBottom:130}} name="favorite-border" size={24} color="white" />
+             <Text style={{color: 'white', marginTop: 40, }}>Air Force 1 Low</Text>
+            <Image style={{
+            marginBottom: 80,  
+            width: 150,
+            height: 90,
+            marginLeft: -125,
+            }} 
+            source={require('../assets/images/wheat and purple.png')} />
+           </TouchableOpacity>
+           </View>
+            </View>
+            </View>
         </View>
 
 
@@ -124,7 +259,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
       },
       imageTitle: {
-        color: 'white',
+        color: 'blue',
         backgroundColor: 'transparent',
         fontSize: 24,
       },
