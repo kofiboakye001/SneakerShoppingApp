d995de195e6c467f0ecfbcd49d8ae912190d3734
Kofi Boakye <kofiboakye2002@gmail.com>
2021-11-17 17:30:29 +0000

    19th

--- screens/Cart.js
+++ screens/Cart.js
@@ -89,6 +89,14 @@ export default function Cart() {
             }}> 
             <Image style={{alignContent: 'center', marginLeft:50, width: 190,height: 50}}source={require('../assets/images/paypal.png')} />
             </TouchableOpacity>
+
+            <TouchableOpacity style={{ backgroundColor: "#f4f5f7", paddingHorizontal: 50, padding: 4,marginRight: 10,marginLeft: 10,marginBottom: 20,
+            flexDirection: "row",
+            borderRadius: 13,
+            }}> 
+            <Image style={{alignContent: 'center', marginLeft:40, width: 80,height:65}}source={require('../assets/images/ms.png')} />
+            <Image style={{alignContent: 'center', marginLeft:30, marginTop: 10, width: 130,height:40}}source={require('../assets/images/visa.png')} />
+            </TouchableOpacity>
                  </ScrollView>
                 </View>
   );
